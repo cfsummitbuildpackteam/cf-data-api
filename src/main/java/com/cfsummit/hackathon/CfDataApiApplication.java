ce3a86f27afb72730374933e2548dfa15d73c736
MichalT <cfsummitsa@gmail.com>
2017-06-13 12:23:46 -0700

    Initial commit

---
+++ src/main/java/com/cfsummit/hackathon/CfDataApiApplication.java
@@ -0,0 +1,12 @@
+package com.cfsummit.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDataApiApplication.class, args);
+	}
+}
